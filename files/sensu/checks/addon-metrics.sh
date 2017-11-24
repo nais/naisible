19ba5dc1faf1cbf4528b33814197d34a7c199edc
H141513 <even.haasted@nav.no>
2017-11-24 16:11:10 +0100

    "Fixed repliaca test also for sensu tests"

--- files/sensu/checks/addon-metrics.sh
+++ files/sensu/checks/addon-metrics.sh
@@ -7,7 +7,7 @@
 ADDON=$1
 TIMESTAMP=$(date '+%s')
 
-if [[ $(/usr/bin/kubectl -n kube-system get deploy ${ADDON} -o json | grep '\"availableReplicas\": 1' | wc -l) == 1 ]]; then
+if [[ $(/usr/bin/kubectl -n kube-system get deploy ${ADDON} -o json | grep availableReplicas | grep -v 0 | wc -l) == 1 ]]; then
     echo "nais.addon.eventtags.addon.${ADDON} 0 ${TIMESTAMP}"
     exit 0
 else

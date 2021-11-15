6dc968a06d872d1f6f3e8da653f6239827249710
Muni10 <sten.ivar.rokke@nav.no>
2021-11-15 11:32:05 +0100

    Updated to etcd 3.5.1 and changed backup script to correspond

--- files/etcd/etcd_backup.sh
+++ files/etcd/etcd_backup.sh
@@ -11,9 +11,9 @@ PROM_CLUSTER=$(sed -nr '/initial-cluster-token/ s/.* (\S+)-etcd.*/\1/p' /etc/sys
 echo "etcd_backup_running 1" | curl -k --data-binary @- https://prometheus-pushgateway.$(echo $PROM_DOMAIN).$(hostname -d)/metrics/job/etcd/instance/$(hostname -f)/cluster/$PROM_CLUSTER
 
 # Clean up old backups (older than three days)
-if [ "$(ls ${BACKUP_DIR} | wc -w)" -gt "3" ]; then
-  find ${BACKUP_DIR} -type d -mtime +3 -delete
-  find ${BACKUP_DIR} -type f -mtime +3 -delete
+if [ "$(ls ${BACKUP_DIR} | wc -w)" -gt "7" ]; then
+  find ${BACKUP_DIR} -type d -mtime +7 -delete
+  find ${BACKUP_DIR} -type f -mtime +7 -delete
 fi
 
 # Sleeping for some nodes to avoid downtime
@@ -28,7 +28,7 @@ systemctl stop etcd || exit 1
 
 # Creating backup with v2 and v3 data
 mkdir -p ${BACKUP_DIR}/${DATE}
-${ETCDCTL_BIN} backup --data-dir ${DATA_DIR} --backup-dir ${BACKUP_DIR}/${DATE} --with-v3 >> ${BACKUP_DIR}/etcd_backup.log 2>&1
+${ETCDCTL_BIN} snapshot save ${BACKUP_DIR}/${DATE}/backupdb >> ${BACKUP_DIR}/etcd_backup.log 2>&1
 
 # Push backup status to prometheus
 if [ "$?" == "0" ]; then
@@ -41,7 +41,7 @@ fi
 systemctl start etcd || exit 1
 
 # Compress backup
-tar -C ${BACKUP_DIR}/${DATE} -czf ${BACKUP_DIR}/${DATE}.tar.gz member && rm -rf ${BACKUP_DIR}/${DATE}
+tar -C ${BACKUP_DIR}/${DATE} -czf ${BACKUP_DIR}/${DATE}.tar.gz backupdb && rm -rf ${BACKUP_DIR}/${DATE}
 
 # Push metric for backup done
 echo "etcd_backup_running 0" | curl -k --data-binary @- https://prometheus-pushgateway.$(echo $PROM_DOMAIN).$(hostname -d)/metrics/job/etcd/instance/$(hostname -f)/cluster/$PROM_CLUSTER
